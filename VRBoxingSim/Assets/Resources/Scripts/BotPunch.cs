4ee353ce6b1c52c407dfad8de3c880a26cc1ce93
SenseiChipstuttar <viktor.fackler@elev.ga.lbs.se>
2025-02-13 10:20:36 +0100

    Animation och punch

---
+++ VRBoxingSim/Assets/Resources/Scripts/BotPunch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotPunch : MonoBehaviour
+{
+    public Transform rightTarget;
+    public Transform rightHint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
